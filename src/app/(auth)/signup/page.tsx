5c4d333af832e273d8746d85d10bba2c21ff8462
dLuxKid <adetunjimarvellous09@gmail.com>
2025-09-30 11:00:44 +0100

    Remove redirect to login page from signup component to allow direct access for users. Cleaned up the code for improved readability.

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,10 +1,7 @@
 import Signup from "@/component/auth/signup";
-import { redirect } from "next/navigation";
 import { Suspense } from "react";
 
 export default function page() {
-  redirect("/login");
-
   return (
     <Suspense>
       <Signup />
